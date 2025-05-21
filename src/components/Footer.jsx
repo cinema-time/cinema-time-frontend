3b2c15dfe683bd86fccb9584cc3a87afe4f39b4c
“nekosaege” <nekofreiheit@gmx.de>
2025-05-21 08:52:05 +0200

    start some changes at Navbar, Footer, About and App.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return (
+        <div>
+<p style={{ color: 'green' }}>What can I say about this Footer? It will also be really nice one day.</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function NavBar(){
+    return (
+        <div>
+<p style={{ color: 'green' }}>this will be a nice Navbar</p>
+        </div>
+    )
+}
+
+export default NavBar
