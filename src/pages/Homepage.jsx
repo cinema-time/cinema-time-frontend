5799079ca67b944ef8409e8ba6ad0c4c932f9c07
bebopmot <bebopmotinc@gmail.com>
2025-05-23 15:55:58 +0200

    update homepage last tweaks

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Carousel CSS
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-
 
 const API_URL = "http://localhost:5005";
 
@@ -22,17 +21,19 @@ function HomePage() {
     <div className="carousel-container">
       <h1>Welcome to Cinema Time</h1>
       <h2>NOW SHOWING</h2>
-      <Carousel autoPlay interval={3000} infiniteLoop>
-        {film.map((filmObj) => (
-          <div key={filmObj.id} className="card">
-            <img src={filmObj.image} alt={filmObj.title} />
-            <div className="card-content">
-              <h3>{filmObj.title}</h3>
-              <p>{filmObj.description}</p>
+      {film.length > 0 && (
+        <Carousel autoPlay interval={3000} infiniteLoop showThumbs={false}>
+          {film.map((filmObj) => (
+            <div key={filmObj.id} className="card">
+              <img src={filmObj.image} alt={filmObj.title} />
+              <div className="card-content">
+                <h3>{filmObj.title}</h3>
+                <p>{filmObj.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </Carousel>
+          ))}
+        </Carousel>
+      )}
     </div>
   );
 }
