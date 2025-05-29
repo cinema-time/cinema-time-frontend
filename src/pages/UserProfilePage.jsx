1b3107c182c20015d498ff544b19d48be669a357
bebopmot <bebopmotinc@gmail.com>
2025-05-29 13:15:33 +0200

    Edit profile bug fix

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import placeholderImage from "./../assets/placeholder.png";
 
-const API_URL = "http://localhost:5005/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function UserProfilePage() {
   const [user, setUser] = useState(null);
